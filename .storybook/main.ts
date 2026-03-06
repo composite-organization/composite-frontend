c657d78fa0bc01218f3ed51c1bcf7692b147e7aa
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-03-06 13:49:36 +0900

    refactor: storybook tailwind 값 HMR로 반영되도록 개선

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  async viteFinal(viteConfig) {
+    const { mergeConfig } = await import('vite');
+    return mergeConfig(viteConfig, {
+      plugins: [tailwindcss()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+        },
+      },
+    });
   },
 };
 
