8a46fc896c5d6793c460710acd78356a19062376
moo1677 <lgm04@naver.com>
2026-03-13 00:23:37 +0900

    style: Modal 컴포넌트 gap 추가

--- src/shared/components/ui/modal/Modal.tsx
+++ src/shared/components/ui/modal/Modal.tsx
@@ -11,12 +11,12 @@ interface ModalProps {
 function Modal({ title, isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
   return (
-    <div className="flex flex-col items-center justify-between w-[500px] h-auto p-8 rounded-4xl bg-black-0">
+    <div className="flex flex-col items-center justify-between w-[500px] h-auto p-8 rounded-4xl bg-black-0 gap-4">
       <div className="flex items-center justify-between w-full h-[52px]">
         <h1 className="h2-semibold">{title}</h1>
         <IconButton iconName="close" shape="circle" onClick={onClose} />
       </div>
-      {children}
+      <div className="flex flex-col w-full gap-6">{children}</div>
     </div>
   );
 }
