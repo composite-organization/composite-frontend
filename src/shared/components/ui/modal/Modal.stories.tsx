f84953634e75a92b36df6be1ea3738a578205658
moo1677 <lgm04@naver.com>
2026-03-14 17:51:36 +0900

    style: 모달 컴포넌트 백드롭 및 속성 추가

--- src/shared/components/ui/modal/Modal.stories.tsx
+++ src/shared/components/ui/modal/Modal.stories.tsx
@@ -1,28 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Modal from './Modal';
 
-const meta = {
+const meta: Meta<typeof Modal> = {
   title: 'UI/Modal',
   component: Modal,
   parameters: {
     layout: 'fullscreen',
   },
-  decorators: [
-    (Story) => (
-      <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <Story />
-      </div>
-    ),
-  ],
-  args: {
-    title: '모달 제목',
-    isOpen: true,
-    onClose: () => {},
-    children: <div className="h-[200px]" />,
-  },
-} satisfies Meta<typeof Modal>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: () => (
+    <Modal title="모달 제목" isOpen onClose={() => {}}>
+      <div className="h-[200px]" />
+    </Modal>
+  ),
+};
--- src/shared/components/ui/modal/Modal.tsx
+++ src/shared/components/ui/modal/Modal.tsx
@@ -11,12 +11,21 @@ interface ModalProps {
 function Modal({ title, isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
   return (
-    <div className="flex flex-col items-center justify-between w-[500px] h-auto p-8 rounded-4xl bg-black-0 gap-4">
-      <div className="flex items-center justify-between w-full h-[52px]">
-        <h1 className="h2-semibold">{title}</h1>
-        <IconButton iconName="close" shape="circle" onClick={onClose} />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black-100 backdrop-blur-sm">
+      <div
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-title"
+        className="flex flex-col items-center w-[500px] h-auto p-8 rounded-[28px] bg-black-0 "
+      >
+        <div className="flex items-center justify-between w-full h-[52px]">
+          <h1 id="modal-title" className="h2-semibold">
+            {title}
+          </h1>
+          <IconButton iconName="close" shape="circle" onClick={onClose} />
+        </div>
+        {children}
       </div>
-      <div className="flex flex-col w-full gap-6">{children}</div>
     </div>
   );
 }
