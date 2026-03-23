0992f87092a2af8106ac1ac1b9eeffd992001e7c
changwooj <wjdckddn99@naver.com>
2026-03-23 14:14:02 +0900

    refactor: story 파일 위치 이동

--- src/features/vote/ui/story/VoteStudent.stories.tsx
+++ src/features/vote/ui/story/VoteStudent.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { VoteStudent } from './ui';
+import { VoteStudent } from '..';
 
 const SAMPLE_SELECTIONS_BASIC = [
   { id: '1', label: '찬성' },
