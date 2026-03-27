71edee4b2b9cd4566cfda57167a75994f42863ef
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-03-27 13:57:33 +0900

    chore: msw 분기 처리

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import App from './app/App';
 
 async function enableMocking() {
-  if (import.meta.env.DEV) {
+  if (import.meta.env.DEV && import.meta.env.VITE_MSW_ENABLED !== 'false') {
     const { worker } = await import('./mocks/browser');
     await worker.start({ onUnhandledRequest: 'bypass' });
   }
